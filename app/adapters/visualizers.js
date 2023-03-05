42f636b1e5300371f8825f47f6d0dcf0ee83a340
Akash Deep <61289472+akash2408@users.noreply.github.com>
2023-03-05 16:34:58 +0530

    change api url

--- app/adapters/visualizers.js
+++ app/adapters/visualizers.js
@@ -1,7 +1,6 @@
-import RESTAdapter from '@ember-data/adapter/json-api';
+import RESTAdapter from "@ember-data/adapter/json-api";
 
 export default class VisualizersAdapter extends RESTAdapter {
-
-  host= "https://api-ds-visualizers.herokuapp.com";
-  namespace= "api"
+  host = "https://visualizer-backend-production.up.railway.app";
+  namespace = "api";
 }
--- app/authenticators/token.js
+++ app/authenticators/token.js
@@ -1,52 +1,50 @@
-import Base from 'ember-simple-auth/authenticators/base';
-import { inject as service } from '@ember/service';
+import Base from "ember-simple-auth/authenticators/base";
+import { inject as service } from "@ember/service";
 
 export default Base.extend({
   flashMessages: service(),
 
   async restore(data) {
-   console.log(data);
-   let {token} = data;
-   if(token){
-    return data;}
-   else
-    {throw 'no valid session data';}
-  },
-
-  async authenticate(email,password) {
-   let response = await fetch('https://api-ds-visualizers.herokuapp.com/api/signin',{
-    method:'POST',
-    headers:{
-       'Content-Type': 'application/json'
-     },
-    body: JSON.stringify({
-      user:{email,password}
-      })
-    });
-   if(response.ok)	
-     {   
-        let res = await response.json();
-        this.flashMessages.add({
-            message           : "Login Successfully!",
-            type              : "success",
-           preventDuplicates : true
-        });
-        return res;
-      }
-   else
-    { 
-      let res = await response.json();
-      this.flashMessages.add({
-          message           : res.error,
-          type              : "danger",
-         preventDuplicates : true
-      });
-      throw new Error(res.error)
+    console.log(data);
+    let { token } = data;
+    if (token) {
+      return data;
+    } else {
+      throw "no valid session data";
     }
   },
 
-  async invalidate(data) {
+  async authenticate(email, password) {
+    let response = await fetch(
+      "https://visualizer-backend-production.up.railway.app/api/signin",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: { email, password },
+        }),
+      }
+    );
+    if (response.ok) {
+      let res = await response.json();
+      this.flashMessages.add({
+        message: "Login Successfully!",
+        type: "success",
+        preventDuplicates: true,
+      });
+      return res;
+    } else {
+      let res = await response.json();
+      this.flashMessages.add({
+        message: res.error,
+        type: "danger",
+        preventDuplicates: true,
+      });
+      throw new Error(res.error);
+    }
+  },
 
-  }
-
+  async invalidate() {},
 });
